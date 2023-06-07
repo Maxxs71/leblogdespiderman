c1551e68bc8cbdc109fe4aad12425e49de08523e
pierre <maxence@gmail.com>
2023-06-07 15:26:22 +0200

    Ajout systeme inscription, connexion et deconnexion

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class RegistrationController extends AbstractController
+{
+    /**
+     * Controlleur de la page d'inscription
+     */
+    #[Route('/creer-un-compte/', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            // hydratation de la date d'inscription du nouvel utilisateur
+            $user->setRegistrationDate(new \DateTime());
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+
+            // Message flash de succes
+            $this->addFlash('success', ' Votre compte a bien été crée avec success ! ');
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+}
